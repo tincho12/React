deed9adddc7452a8f66ad7dadbd56481db97e8ac
tincho <tincho@gmail.com>
2020-07-27 18:30:03 -0300

    State

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import React from 'react';
 import './App.css';
 
-function Helloworld (objProps) {
-  return <div id="hello">
-            <h3>{objProps.subtitle}</h3>
-            {objProps.mytext}
-          </div>
+
+class Helloworld extends React.Component {
+
+  state = {
+    show: true
+  }
+
+  toggleShow = () => {
+    this.setState({show: !this.state.show})
+  }
+
+  render() {
+
+    if (this.state.show) {
+      return (
+        <div id="hello">
+          <h3>{this.props.subtitle}</h3>
+          {this.props.mytext}
+          <button onClick={this.toggleShow}>Cambiar Estado</button>
+        </div>)
+    } else {
+      return <h1>
+        <button onClick={this.toggleShow}>Cambiar Estado</button>
+        No hay elementos</h1>
+      
+    }
+  }
 }
 
 function App() {
   return (
     <div>this is my component: 
       <Helloworld mytext="Hello Fast" subtitle="loremp ispum"/>
-      <Helloworld mytext="Hello Martin" />
+      <Helloworld mytext="Hello Martin" subtitle="loremp" />
       <Helloworld mytext="Hello ?" subtitle="Hola !!!" />
     </div>
   );
