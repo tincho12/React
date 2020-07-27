30605d3a2fd684d1c9dcc21930365b9aad377e4e
tincho <tincho@gmail.com>
2020-07-27 17:57:12 -0300

    Clase 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import './App.css';
 
+function Helloworld () {
+  return <div id="hello">Hello World</div>
+}
+
+//3 Formas de declarar le mismo componente constante clase y funct
+
+const App = () => <div>This is my component: <Helloworld/></div>
+
+/*
+class App extends React.Component {
+  render() {
+    return <div>This is my component: <Helloworld/></div>
+  }
+}
+*/
+
+/*
 function App() {
   return (
-    <div>Hello World</div>
+    <div>this is my component: <Helloworld/></div>
   );
 }
+*/
 
 export default App;
