04f13719c66eff61be529a9559e74e2baca05d95
tincho <tincho@gmail.com>
2020-07-28 17:50:09 -0300

    styles

--- src/components/task.js
+++ src/components/task.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
+import './task.css';
 
 class Task extends Component {
     render() {
 
         const {tarea} = this.props;
 
-        return <div>
+        return <p className="red">
             {tarea.title} - {tarea.description} - {tarea.done} - {tarea.id}
             <input type="checkbox"/>
-            <button>x</button>
-        </div>
+            <button style={btnDelete}>x</button>
+        </p>
     }
+}
+
+const btnDelete = {
+    fontSize: '18px',
+    background: '#ea2027',
+    color: '#fff',
+    border: 'none',
+    padding: '10px 15px',
+    borderRadius: '50%',
+    cursor: 'pointer',
+    
+
 }
 
 export default Task
